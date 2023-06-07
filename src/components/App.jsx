a56fae1566bd413dbd7178f0acc8ce50fd28057e
YPV0 <yuriisfw@gmail.com>
2023-06-07 21:14:30 +0300

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,13 +1,19 @@
 import { lazy, Suspense } from 'react';
 import { Routes, Route, NavLink } from 'react-router-dom';
 import { NavLinkItem, NavList } from './App.styled';
+import Home from 'pages/home';
+import Movies from 'pages/movies';
+import MovieDetails from 'pages/MovieDetails';
+import Cast from './Cast/Cast';
+import Reviews from './Reviews/Reviews';
+import NotFound from 'pages/NotFound';
 
-const Cast = lazy(() => import('components/Cast/Cast'));
-const Reviews = lazy(() => import('components/Reviews/Reviews'));
-const Home = lazy(() => import('pages/home'));
-const Movies = lazy(() => import('pages/movies'));
-const MovieDetails = lazy(() => import('pages/MovieDetails'));
-const NotFound = lazy(() => import('pages/NotFound'));
+// const Cast = lazy(() => import('components/Cast/Cast'));
+// const Reviews = lazy(() => import('components/Reviews/Reviews'));
+// const Home = lazy(() => import('pages/home'));
+// const Movies = lazy(() => import('pages/movies'));
+// const MovieDetails = lazy(() => import('pages/MovieDetails'));
+// const NotFound = lazy(() => import('pages/NotFound'));
 
 export const App = () => {
   return (
