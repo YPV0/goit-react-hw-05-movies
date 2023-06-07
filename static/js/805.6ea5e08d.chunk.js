61f8ca005ea93fcba098bb2ad7277386a3253e4a
YPV0 <124578390+YPV0@users.noreply.github.com>
2023-06-07 18:31:09 +0000

    Deploying to gh-pages from @ YPV0/goit-react-hw-05-movies@130626a086ebb1d4f7c9bca8fde0047935709d82 🚀

---
+++ static/js/805.6ea5e08d.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[805],{805:function(e,t,n){n.r(t);var r=n(184);t.default=function(){return(0,r.jsxs)("div",{style:{textAlign:"center"},children:[(0,r.jsx)("b",{style:{fontSize:100},children:"404"}),(0,r.jsx)("p",{children:"Sorry, page not found"})]})}}}]);
+//# sourceMappingURL=805.6ea5e08d.chunk.js.map
